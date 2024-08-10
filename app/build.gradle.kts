026c10de76c03cf0168753160bac0008ddd99594
merkost <konstantinlikes@gmail.com>
2024-08-10 18:47:48 +1000

    [release] 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         namespace = "com.mobileprism.fishing"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.1"
+        versionCode = 15
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
